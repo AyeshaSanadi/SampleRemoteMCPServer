8ba398783267ca87f52d7132ffb5c168508e10a0
AyeshaSanadi <ayeshasanadi09@gmail.com>
2026-03-17 11:21:38 +0530

    Added remote server code

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastmcp import FastMCP 
+
+mcp = FastMCP(name="Simple Operation")
+
+@mcp.tool
+def add(n1: int, n2: int) -> int:
+    return n1 + n2
+
+if __name__ == "__main__":
+    mcp.run(transport="http", )
+
